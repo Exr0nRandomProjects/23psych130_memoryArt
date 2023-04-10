c4af3180857b226ec40481108acbf24f808693d3
Exr0n <mail@exr0n.com>
2023-04-10 01:49:20 -0700

    UT: image overlay with opacity

--- camera.py
+++ camera.py
@@ -1,14 +1,39 @@
 
 import cv2
 import base64
+import config
+from importlib import reload
+import numpy as np
 
 cam = cv2.VideoCapture(0)
 
 def get_image():
     ret, image = cam.read()
+    image = overlay_cone(image)
     ret, buf = cv2.imencode('.jpg', image)
     byts = buf.tobytes()
     return base64.b64encode(byts)
+
+def overlay_cone(image):
+    reload(config)
+
+    x_offset = config.OFFSET_LEFT
+    y_offset = config.OFFSET_TOP
+
+    # image blending from https://stackoverflow.com/a/14102014/10372825
+    s_img = cv2.imread("/home/ubuntu/psychmem/cone_image.png", -1)
+
+    y1, y2 = y_offset, y_offset + s_img.shape[0]
+    x1, x2 = x_offset, x_offset + s_img.shape[1]
+
+    alpha_s = s_img[:, :, 3] / 255.0
+    alpha_l = 1.0 - alpha_s
+
+    for c in range(0, 3):
+        image[y1:y2, x1:x2, c] = (alpha_s * s_img[:, :, c] +
+                                  alpha_l * image[y1:y2, x1:x2, c])
+
+    return image
 
 def cleanup():
     cam.release()
--- main.py
+++ main.py
@@ -24,14 +24,17 @@ body {{
 </html>
 '''
 
+
 class Server(BaseHTTPRequestHandler):
+    image_to_send = get_image()
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
         #self.wfile.write(bytes("utterly based", 'utf-8'))
         #self.wfile.write(get_image())
-        self.wfile.write(bytes(big_string.format(imgdata=get_image().decode('utf-8')), 'utf-8'))
+        self.wfile.write(bytes(big_string.format(imgdata=self.image_to_send.decode('utf-8')), 'utf-8'))
+        self.image_to_send = get_image() # send the image from the previous iteration
 
 if __name__ == '__main__':
     ws = HTTPServer((hostname, port), Server)
@@ -44,5 +47,5 @@ if __name__ == '__main__':
     finally:
         ws.server_close()
         cleanup()
-    
+
 
